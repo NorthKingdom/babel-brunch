42b79658dcc65dbfba33b7215c5a3fbbff8f5506
arondesign <aron.chavez@gmail.com>
2015-03-17 09:10:57 -0700

    Changed to parse es6 files instead of js

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ function ES6to5Compiler(config) {
 
 ES6to5Compiler.prototype.brunchPlugin = true;
 ES6to5Compiler.prototype.type = 'javascript';
-ES6to5Compiler.prototype.extension = 'js';
+ES6to5Compiler.prototype.extension = 'es6';
 
 ES6to5Compiler.prototype.compile = function (params, callback) {
   if (this.isIgnored(params.path)) return callback(null, params);
